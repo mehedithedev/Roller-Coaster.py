0ddfb7f26f0fb603f98de1d6b94f0d06931ca0d9
Mehedi Hasan <mehedihasan2882@icloud.com>
2022-11-16 13:18:00 +0600

    Update Roller_Coaster.py

--- Roller_Coaster.py
+++ Roller_Coaster.py
@@ -2,5 +2,10 @@ print("Welcome to the Roller Coaster")
 height=float(input("What's your height in feet? "))
 if height>=5 :
     print("You are eligible to ride on the Roller Coaster!")
+    age=int(input("How old are you? "))
+    if age <= 18:
+        print("You have to pay $8 to proceed further.")
+    else:
+        print("You have to pay adult fees, which is $16 in order to proceed further.")
 else:
-    print("You need to wait more to achive the required height.")
+    print("You have to wait more to achive the required height.")
