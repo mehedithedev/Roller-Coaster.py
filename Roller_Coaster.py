fe5761218650f6c9399fc8fadefa2b8bc9e94f88
Mehedi Hasan <mehedihasan2882@icloud.com>
2022-11-16 12:24:39 +0600

    Create Roller_Coaster.py

---
+++ Roller_Coaster.py
@@ -0,0 +1,6 @@
+print("Welcome to the Roller Coaster")
+height=float(input("What's your height in feet? "))
+if height>=5 :
+    print("You are eligible to ride on the Roller Coaster!")
+else:
+    print("You need to wait more to achive the required height.")
